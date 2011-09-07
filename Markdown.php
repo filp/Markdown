530ffcfd64d2b8cccf7469913f32a5ea99113ef9
Filipe Dobreira <dobreira@gmail.com>
2011-09-07 14:19:50 +0100

    Added license information.

--- Markdown.php
+++ Markdown.php
@@ -1,6 +1,19 @@
 <?php
 
 /**
+ *	Markdown.php is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Markdown.php is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Markdown.php. If not, see <http://www.gnu.org/licenses/>.
+ *
  *	Markdown
  *
  *	Markdown  -  A text-to-HTML conversion tool for web writers
